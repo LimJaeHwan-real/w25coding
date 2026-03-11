67810b29631c1edaae410f26e160815a06d26b87
LimJaeHwan <shawncan1573@gmail.com>
2026-03-11 09:49:07 +0900

    3/10 문제풀이

--- week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
+++ week2/problem-solving/난이도중_백트래킹_외판원순회2_실버2.py
@@ -1,2 +1,35 @@
 # 백트래킹 - 외판원 순회 2 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10971
+
+import sys
+read=sys.stdin.readline
+
+n=int(read().strip())
+arr=[[0 for _ in range(n)] for _ in range(n)]
+visited=[0]*n
+
+for i in range(n):
+    arr[i]=list(map(int,read().strip().split()))
+
+
+mn=sys.maxsize
+def sol(cnt,start,dist,history):
+    global mn
+    if cnt==(n-1) and arr[start][0]:
+        mn=min(mn,dist+arr[start][0])
+        print(history)
+        return
+    
+    for i in range(n):
+        if not visited[i] and arr[start][i]:
+            visited[i]=1
+            sol(cnt+1,i,dist+arr[start][i],history+[i])
+            visited[i]=0
+
+visited[0]=1
+sol(0,0,0,[0])
+print(mn)
+
+
+
+
--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,7 +1,50 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
 import sys
+from itertools import permutations
 read=sys.stdin.readline
+
+n=int(read())
+org_arr=list(map(int,read().strip().split()))
+
+perm=list(permutations(org_arr,n))
+result=0
+for arr in perm:
+    result=max( result,sum( [abs(arr[i-1]-arr[i]) for i in range(1,n)] ) )
+print(result)
+
+
+
+
+# # 백트래킹 방법
+# visited=[0]*n
+# # index (1~n-1) 까지 양쪽 옆 요소와의 차를 통해 값을 얻을 수 있다
+# result=0
+
+# def ssum(arr):
+#     sum=0
+#     for i in range(1,n):
+#         sum+=abs(arr[i-1]-arr[i])
+#     return sum
+
+# def re(arr):
+#     global result
+#     if len(arr)==n:
+#         result=max(result,ssum(arr))
+#         return
+#     for i in range(n):
+#         if not visited[i]:
+#             visited[i]=1
+#             re(arr+[org_arr[i]])
+#             visited[i]=0
+
+
+# re([])
+# print(result)
+
+
+
+
 
 
 

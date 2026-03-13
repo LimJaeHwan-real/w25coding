41d067889e45aa9c8e5bff3636d68c1a6c0627c3
LimJaeHwan-real <shawncan1573@gmail.com>
2026-03-13 23:51:43 +0900

    3/13 백준 문제 풀이

--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,19 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+import sys
+read=sys.stdin.readline
+a,b,c=list(map(int,read().strip().split()))
+
+def sol(base,exp,div):
+    if exp==1:
+        return base%div
+    val=sol(base,exp//2,div)%div
+
+    if exp%2==0:
+        return val*val%div
+    else:
+        return base*val*val%div
+
+print(sol(a,b,c))
+    
+
--- week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
+++ week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
@@ -1,2 +1,43 @@
 # 분할정복 - 색종이 만들기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/2630
+import sys
+read=sys.stdin.readline
+WHITE='0'
+BLUE='1'
+
+n=int(read().strip())
+board=[]
+for i in range(n):
+    board.append(read().strip().split())
+
+def is_all(y,x,m,color):
+    for i in range(y,y+m):
+        for j in range(x,x+m):
+            if board[i][j]!=color:
+                return False
+    return True
+    
+    
+    
+blue_cnt=0
+white_cnt=0
+def sol(y,x,m):
+    global blue_cnt,white_cnt
+    if is_all(y,x,m,WHITE):
+        white_cnt+=1
+        return
+    if is_all(y,x,m,BLUE):
+        blue_cnt+=1
+        return
+    half=m//2
+    sol(y,x,half)
+    sol(y,x+half,half)
+    sol(y+half,x,half)
+    sol(y+half,x+half,half)
+   
+sol(0,0,n)
+print(white_cnt)    
+print(blue_cnt)
+
+    
+    

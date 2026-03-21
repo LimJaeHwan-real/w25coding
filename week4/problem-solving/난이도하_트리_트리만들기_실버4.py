25f0b5a681d7b8d8ddb10f302878a8ca8fb722ef
LimJaeHwan-real <shawncan1573@gmail.com>
2026-03-21 14:16:48 +0900

    boj 14244 트리만들기

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,14 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+import sys
+from collections import defaultdict
+read=sys.stdin.readline
+n,m=list(map(int,read().rstrip().split()))
+
+
+for i in range(n-m+1):
+    print(i,i+1)
+
+for i in range(n-m+2,n):
+    print(1,i)
+

2ed8d83d620906dd5319d975e3f9e9ba761dd5e6
LimJaeHwan-real <shawncan1573@gmail.com>
2026-03-21 14:49:01 +0900

    boj9372 상근이의여행

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,34 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+import sys
+read=sys.stdin.readline
+# 트리에서 모든 노드를 연결하는 최소 간선의 갯수를 구하여라
+
+t=int(read().strip())
+for _ in range(t):
+    n,m=list(map(int,read().rstrip().split()))
+    for _ in range(m):
+        u,v=list(map(int,read().rstrip().split()))
+    
+    print(n-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

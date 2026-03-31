37010ee7384fac88932db96c89df734723bad2d8
LimJaeHwan-real <shawncan1573@gmail.com>
2026-03-31 10:04:38 +0900

    0330 solved

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,17 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+import sys
+read=sys.stdin.readline
+dp=[[0]*1002 for _ in range(1002)]
+str1=read().strip()
+str2=read().strip()
+
+
+for i in range(1,len(str1)+1):
+    for j in range(1,len(str2)+1):
+        if str1[i-1]!=str2[j-1]:
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        else:
+            dp[i][j]=dp[i-1][j-1]+1
+     
+print(dp[len(str1)][len(str2)])
--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,20 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+import sys
+read=sys.stdin.readline
+t=int(read().strip())
+
+for _ in range(t):
+    n=int(read().strip())
+    coins=list(map(int,read().strip().split()))
+    goal=int(read().strip())
+    
+    dp=[0]*(goal+1)
+    dp[0]=1
+    for coin in coins:
+        for money in range(coin, goal+1):
+            dp[money] += dp[money - coin]
+    
+
+    print(dp[goal])
+
